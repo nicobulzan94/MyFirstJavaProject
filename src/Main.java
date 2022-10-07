2faa2a6377bdd2e36e836dcb94288ff7aec28cca
Nicoleta Bulzan <nicoletabulzan94@gmail.com>
2022-10-07 22:01:22 +0300

    Tema Laborator 9 InteliJ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,42 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        //1. Printati "Hello" in consola, iar pe rand nou, printati numele vostru.
+
+        String start = "Hello\n" + "Nico";
+        System.out.println(start);
+
+
+        //2. Printati rezultatul sumei a doua numere(orice numere)
+
+        short MySumVar = 835 + 56;
+        System.out.println(MySumVar);
+
+
+        //3. Printati rezultatul impartirii a doua numere. Atentie, daca numarul nu este intreg, sa afiseze si valorile dupa virgula.
+
+        int divideVariable = 9987/2;
+        System.out.println(divideVariable);
+
+
+        //4. Printati rezultatul urmatoarelor operatiuni:
+
+        byte a = -5 + 8 * 6;
+        byte b = (55+9) % 9;
+        byte c = 20 + -3*5 / 8;
+        byte d = 5 + 15 / 3 * 2 - 8 % 3;
+
+        System.out.println(a + " " + b + " " + c + " " + d);
+
+
+
+
+
+
+
+
     }
 }
